44aad3553e04d0ea3853676ec7ebc2aa1f2bbdb5
Vadim Kasatkin <penguin2804@gmail.com>
2023-04-17 23:22:42 +0400

    refactor: add lang for mycomponent

---
+++ local/components/ex2/simplecomp.exam/templates/.default/lang/ru/template.php
@@ -0,0 +1,6 @@
+<?php
+
+$MESS["COMPONENT_TITLE"] = "Элеметнов";
+$MESS["COMPONENT_LIST_TITLE"] = "Каталог";
+$MESS["BLOCK_MAX_PRICE"] = "Максимальная цена";
+$MESS["BLOCK_MIN_PRICE"] = "Минимальная цена";
--- local/components/ex2/simplecomp.exam/templates/.default/template.php
+++ local/components/ex2/simplecomp.exam/templates/.default/template.php
@@ -2,11 +2,13 @@
 /** @var array $arResult */
 /** @var CBitrixComponentTemplate $this */
 
+use Bitrix\Main\Localization\Loc;
+
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 ?>
-<h3>Элеметнов - <?=$arResult["TOTAL_CNT"]?></h3>
+<h3><?=Loc::GetMessage("COMPONENT_TITLE");?> - <?=$arResult["TOTAL_CNT"]?></h3>
 ----
-<p><b>Каталог:</b></p>
+<p><b><?=Loc::GetMessage("COMPONENT_LIST_TITLE");?>:</b></p>
 <ul>
 <?php foreach ($arResult["ITEMS"] as $item):?>
     <li>
@@ -33,7 +35,7 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 
 <?php $this->SetViewTarget("price_block");?>
 <div style="color:red; margin: 34px 15px 35px 15px">
-    <p>Максимальная цена: <?=$arResult["PRODUCT_MAX_PRICE"]?></p>
-    <p>Минимальная цена: <?=$arResult["PRODUCT_MIN_PRICE"]?></p>
+    <p><?=Loc::GetMessage("BLOCK_MAX_PRICE");?>: <?=$arResult["PRODUCT_MAX_PRICE"]?></p>
+    <p><?=Loc::GetMessage("BLOCK_MIN_PRICE");?>: <?=$arResult["PRODUCT_MIN_PRICE"]?></p>
 </div>
 <?php $this->EndViewTarget();?>
