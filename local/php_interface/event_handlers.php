b7e497b8a507a2150ebb39cb3ceec4f0c85b12d4
Vadim Kasatkin <penguin2804@gmail.com>
2023-04-17 17:09:37 +0400

    feat: ex2-95

---
+++ local/php_interface/event_handlers.php
@@ -0,0 +1,25 @@
+<?php
+
+AddEventHandler("main", "OnBuildGlobalMenu", ["EventHandlers", "OnBuildGlobalMenu"]);
+class EventHandlers
+{
+    public function OnBuildGlobalMenu(&$aGlobalMenu, &$aModuleMenu)
+    {
+        global $USER;
+        $contentManagersGroupID = 5;
+        if ($USER->IsAdmin() && !in_array($contentManagersGroupID, $USER->GetUserGroupArray())) {
+            return null;
+        }
+
+        foreach ($aModuleMenu as $item) {
+            if ($item["text"] === "Новости") {
+                $aModuleMenu = [$item];
+                break;
+            }
+        }
+
+        $aGlobalMenu = [
+            "global_menu_content" => $aGlobalMenu["global_menu_content"]
+        ];
+    }
+}
