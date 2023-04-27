d51e983034ea54f313d1473041021b36574981a1
Vadim Kasatkin <penguin2804@gmail.com>
2023-04-27 16:10:06 +0400

    feat: ex2-94

--- local/php_interface/event_handlers.php
+++ local/php_interface/event_handlers.php
@@ -11,7 +11,9 @@ class MyClass
             $elem = CIBlockElement::GetByID($arParams["ID"])->Fetch();
             if ($elem["ACTIVE"] === "Y" && (int)$elem["SHOW_COUNTER"] > 2) {
                 global $APPLICATION;
-                $APPLICATION->throwException("Товар невозможно деактивировать, у него {$elem["SHOW_COUNTER"]} просмотров");
+                $APPLICATION->throwException(
+                    "Товар невозможно деактивировать, у него {$elem["SHOW_COUNTER"]} просмотров"
+                );
                 return false;
             }
         }
@@ -19,11 +21,12 @@ class MyClass
 
     public static function onEpilog()
     {
+        global $APPLICATION;
+
         if (constant('ERROR_404') === "Y") {
-            global $APPLICATION;
             $APPLICATION->RestartBuffer();
             CHTTP::SetStatus("404 Not Found");
-            include($_SERVER["DOCUMENT_ROOT"].SITE_TEMPLATE_PATH."/header.php");
+            include($_SERVER["DOCUMENT_ROOT"] . SITE_TEMPLATE_PATH . "/header.php");
             include($_SERVER["DOCUMENT_ROOT"] . '/404.php');
             include($_SERVER["DOCUMENT_ROOT"] . SITE_TEMPLATE_PATH . "/footer.php");
             CEventLog::Add(array(
@@ -32,6 +35,21 @@ class MyClass
                 "MODULE_ID" => "main",
                 "DESCRIPTION" => "url страницы",
             ));
+            exit;
+        }
+
+        if (CModule::IncludeModule("iblock")) {
+            $arFilter = ["IBLOCK_ID" => 6, "NAME" => $APPLICATION->GetCurPage()];
+            $selectedFields = ["PROPERTY_TITLE", "PROPERTY_DESCRIPTION"];
+            $metaTagsQ = CIBlockElement::GetList(false, $arFilter, false, false, $selectedFields);
+            if ($pageMetaTags = $metaTagsQ->Fetch()) {
+                if ($pageMetaTags["PROPERTY_TITLE_VALUE"]) {
+                    $APPLICATION->SetPageProperty("title", $pageMetaTags["PROPERTY_TITLE_VALUE"]);
+                }
+                if ($pageMetaTags["PROPERTY_DESCRIPTION_VALUE"]) {
+                    $APPLICATION->SetPageProperty("description", $pageMetaTags["PROPERTY_DESCRIPTION_VALUE"]);
+                }
+            }
         }
     }
 }
