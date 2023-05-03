bd494e553d0fa5e156a4f8af982d3888247eb9b0
Vadim Kasatkin <penguin2804@gmail.com>
2023-05-03 18:30:58 +0400

    feat: ex2-88

--- ex2/.left.menu.php
+++ ex2/.left.menu.php
@@ -6,6 +6,13 @@ $aMenuLinks = Array(
 		Array(), 
 		Array(), 
 		"" 
+	),
+	Array(
+		"Оценка производительности", 
+		"/ex2/time_control/", 
+		Array(), 
+		Array(), 
+		"" 
 	)
 );
 ?>
---
+++ ex2/time_control/index.php
@@ -0,0 +1,6 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Оценка производительности");
+?>Сама ресурсоёмкая страница&nbsp;<a href="http://exam2.loc/bitrix/admin/perfmon_hit_list.php?lang=ru&set_filter=Y&find_script_name=%2Fproducts%2Findex.php">/products/index.php</a>&nbsp;доля: 30%<br>
+ Размер кеша по умолчанию: 25кб<br>
+ Размер кеша с доп.данными: 27кб<br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
