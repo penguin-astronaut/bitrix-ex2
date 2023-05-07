4a06c7a5c3d6f12c628a44102d7880ff74e08f53
Vadim Kasatkin <penguin2804@gmail.com>
2023-05-07 16:09:26 +0400

    fix condition

--- local/php_interface/event_handlers.php
+++ local/php_interface/event_handlers.php
@@ -7,7 +7,7 @@ class EventHandlers
     {
         global $USER;
         $contentManagersGroupID = 5;
-        if (!$USER->IsAdmin() && !in_array($contentManagersGroupID, $USER->GetUserGroupArray())) {
+        if (!$USER->IsAdmin() && in_array($contentManagersGroupID, $USER->GetUserGroupArray())) {
             foreach ($aModuleMenu as $item) {
                 if ($item["text"] === "Новости") {
                     $aModuleMenu = [$item];
